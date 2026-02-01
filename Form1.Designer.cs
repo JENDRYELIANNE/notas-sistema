fbf9e9cbc2b5210685030d8789e6b81928dcfc46
JENDRYELIANNE <jendryeliannyjamestorres@gmail.com>
2026-01-31 21:20:43 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace notas_sistema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.extratxt = new System.Windows.Forms.TextBox();
+            this.completxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cerrarbtn = new System.Windows.Forms.Button();
+            this.limpiarbtm = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.calcularbtm = new System.Windows.Forms.Button();
+            this.n3txt = new System.Windows.Forms.TextBox();
+            this.n4txt = new System.Windows.Forms.TextBox();
+            this.respuestatxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.prometxt = new System.Windows.Forms.TextBox();
+            this.n1txt = new System.Windows.Forms.TextBox();
+            this.n2txt = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.extratxt);
+            this.groupBox1.Controls.Add(this.completxt);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(-6, 298);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox1.Size = new System.Drawing.Size(579, 198);
+            this.groupBox1.TabIndex = 34;
+            this.groupBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(15, 43);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(154, 32);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Completivo";
+            // 
+            // extratxt
+            // 
+            this.extratxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.extratxt.Location = new System.Drawing.Point(229, 127);
+            this.extratxt.Multiline = true;
+            this.extratxt.Name = "extratxt";
+            this.extratxt.Size = new System.Drawing.Size(344, 36);
+            this.extratxt.TabIndex = 10;
+            // 
+            // completxt
+            // 
+            this.completxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.completxt.Location = new System.Drawing.Point(229, 35);
+            this.completxt.Multiline = true;
+            this.completxt.Name = "completxt";
+            this.completxt.Size = new System.Drawing.Size(344, 33);
+            this.completxt.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(15, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(210, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Extraordinario";
+            // 
+            // cerrarbtn
+            // 
+            this.cerrarbtn.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.cerrarbtn.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cerrarbtn.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.cerrarbtn.Location = new System.Drawing.Point(289, 614);
+            this.cerrarbtn.Name = "cerrarbtn";
+            this.cerrarbtn.Size = new System.Drawing.Size(189, 46);
+            this.cerrarbtn.TabIndex = 33;
+            this.cerrarbtn.Text = "Cerrar";
+            this.cerrarbtn.UseVisualStyleBackColor = false;
+            this.cerrarbtn.Click += new System.EventHandler(this.cerrarbtn_Click);
+            // 
+            // limpiarbtm
+            // 
+            this.limpiarbtm.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.limpiarbtm.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.limpiarbtm.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.limpiarbtm.Location = new System.Drawing.Point(46, 614);
+            this.limpiarbtm.Name = "limpiarbtm";
+            this.limpiarbtm.Size = new System.Drawing.Size(170, 46);
+            this.limpiarbtm.TabIndex = 32;
+            this.limpiarbtm.Text = "limpiar";
+            this.limpiarbtm.UseVisualStyleBackColor = false;
+            this.limpiarbtm.Click += new System.EventHandler(this.limpiarbtm_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(20, 560);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 29);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Respuesta:";
+            // 
+            // calcularbtm
+            // 
+            this.calcularbtm.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.calcularbtm.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.calcularbtm.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcularbtm.Location = new System.Drawing.Point(12, 250);
+            this.calcularbtm.Name = "calcularbtm";
+            this.calcularbtm.Size = new System.Drawing.Size(530, 48);
+            this.calcularbtm.TabIndex = 30;
+            this.calcularbtm.Text = "Carcular ";
+            this.calcularbtm.UseVisualStyleBackColor = false;
+            this.calcularbtm.Click += new System.EventHandler(this.calcularbtm_Click);
+            // 
+            // n3txt
+            // 
+            this.n3txt.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n3txt.Location = new System.Drawing.Point(130, 143);
+            this.n3txt.Name = "n3txt";
+            this.n3txt.Size = new System.Drawing.Size(83, 36);
+            this.n3txt.TabIndex = 29;
+            // 
+            // n4txt
+            // 
+            this.n4txt.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n4txt.Location = new System.Drawing.Point(130, 199);
+            this.n4txt.Name = "n4txt";
+            this.n4txt.Size = new System.Drawing.Size(83, 36);
+            this.n4txt.TabIndex = 28;
+            // 
+            // respuestatxt
+            // 
+            this.respuestatxt.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.respuestatxt.Location = new System.Drawing.Point(151, 549);
+            this.respuestatxt.Multiline = true;
+            this.respuestatxt.Name = "respuestatxt";
+            this.respuestatxt.Size = new System.Drawing.Size(312, 40);
+            this.respuestatxt.TabIndex = 27;
+            this.respuestatxt.TextChanged += new System.EventHandler(this.respuestatxt_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 29);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Nota 4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 29);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Nota 3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 29);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Nota 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 29);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Nota 1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.groupBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.prometxt);
+            this.groupBox2.Controls.Add(this.n1txt);
+            this.groupBox2.Controls.Add(this.n2txt);
+            this.groupBox2.Controls.Add(this.n4txt);
+            this.groupBox2.Controls.Add(this.calcularbtm);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.n3txt);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(0, -6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox2.Size = new System.Drawing.Size(573, 368);
+            this.groupBox2.TabIndex = 35;
+            this.groupBox2.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.label8.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(326, 101);
+            this.label8.Name = "label8";
+            this.label8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label8.Size = new System.Drawing.Size(175, 29);
+            this.label8.TabIndex = 19;
+            this.label8.Text = " su promedio fue de:";
+            // 
+            // prometxt
+            // 
+            this.prometxt.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prometxt.Location = new System.Drawing.Point(331, 136);
+            this.prometxt.Name = "prometxt";
+            this.prometxt.Size = new System.Drawing.Size(169, 36);
+            this.prometxt.TabIndex = 20;
+            this.prometxt.TextChanged += new System.EventHandler(this.prometxt_TextChanged);
+            // 
+            // n1txt
+            // 
+            this.n1txt.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n1txt.Location = new System.Drawing.Point(132, 90);
+            this.n1txt.Multiline = true;
+            this.n1txt.Name = "n1txt";
+            this.n1txt.Size = new System.Drawing.Size(83, 40);
+            this.n1txt.TabIndex = 7;
+            // 
+            // n2txt
+            // 
+            this.n2txt.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n2txt.Location = new System.Drawing.Point(132, 44);
+            this.n2txt.Name = "n2txt";
+            this.n2txt.Size = new System.Drawing.Size(83, 36);
+            this.n2txt.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 672);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cerrarbtn);
+            this.Controls.Add(this.limpiarbtm);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.respuestatxt);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox extratxt;
+        private System.Windows.Forms.TextBox completxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button cerrarbtn;
+        private System.Windows.Forms.Button limpiarbtm;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button calcularbtm;
+        private System.Windows.Forms.TextBox n3txt;
+        private System.Windows.Forms.TextBox n4txt;
+        private System.Windows.Forms.TextBox respuestatxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox prometxt;
+        private System.Windows.Forms.TextBox n1txt;
+        private System.Windows.Forms.TextBox n2txt;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notas_sistema
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cerrarbtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void limpiarbtm_Click(object sender, EventArgs e)
+        {
+            n1txt.Clear();
+            n2txt.Clear();
+            n3txt.Clear();
+            n4txt.Clear();
+            extratxt.Clear();
+            extratxt.Enabled = false;
+            completxt.Clear();
+            completxt.Enabled = false;
+            respuestatxt.Clear();
+            prometxt.Clear();
+            prometxt.Enabled = false;
+            respuestatxt.Enabled = false;
+        }
+
+        private void calcularbtm_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(n1txt.Text);
+            double n2 = double.Parse(n2txt.Text);
+            double n3 = double.Parse(n3txt.Text);
+            double n4 = double.Parse(n4txt.Text);
+
+            double promedio = (n1 + n2 + n3 + n4) / 4;
+            prometxt.Text = promedio.ToString("0.00");
+
+            if (promedio > 69)
+            {
+                respuestatxt.Text = "Aprobado 👍";
+                return;
+            }
+            // complectivo operacion---//
+            respuestatxt.Text = "estas en complectivo !";
+            completxt.Enabled = true;
+
+            if (string.IsNullOrWhiteSpace(completxt.Text)) return;
+
+            double notaCompletivo = double.Parse(completxt.Text);
+            double resultadoCompletivo =
+            (promedio * 0.50) + (notaCompletivo * 0.50);
+
+
+            if (resultadoCompletivo > 69)
+            {
+                respuestatxt.Text = "Aprobado por Completivo 😊";
+                return;
+            }
+            // extraurdinado--///
+            respuestatxt.Text = "Debe ir a Extraordinario❗";
+            extratxt.Enabled = true;
+
+            if (string.IsNullOrWhiteSpace(extratxt.Text)) return;
+
+            double notaExtra = double.Parse(extratxt.Text);
+            double resultadoExtra = (promedio * 0.30) + (notaExtra * 0.70);
+
+            if (resultadoExtra > 69)
+                respuestatxt.Text = "Aprobado por Extraordinario";
+            else
+                respuestatxt.Text = "Reprobado";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            completxt.Enabled = false;
+            extratxt.Enabled = false;
+            prometxt.ReadOnly = true;
+            respuestatxt.ReadOnly = true;
+        }
+
+        private void prometxt_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void respuestatxt_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
